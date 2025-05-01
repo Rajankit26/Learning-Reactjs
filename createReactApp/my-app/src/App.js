071dd3feeadff2458ad6f8800d7c5541d4706e97
Ankit Raj <142731001+Rajankit26@users.noreply.github.com>
2025-05-01 08:52:29 +0000

    create-react-app

---
+++ createReactApp/my-app/src/App.js
@@ -0,0 +1,12 @@
+import Greet from "./hello.js";
+
+function App() {
+  return (
+    <>
+    <h1>Create react app</h1>
+    <Greet />
+    </>
+  );
+}
+
+export default App;
---
+++ createReactApp/my-app/src/hello.js
@@ -0,0 +1,9 @@
+// Function name should start with uppercase
+function Greet(){
+    return (
+        <>
+        <h2>Hello and welcome to React!</h2>
+        </>
+    )
+}
+export default Greet;
