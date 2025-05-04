fa95135789af2818d6534d47436fcf98b77403ad
Ankit Raj <142731001+Rajankit26@users.noreply.github.com>
2025-05-04 10:43:28 +0000

    Update counter multiple times in a single click using functional updates

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    /* Based on old value  -> Simple case, ek hi baar update
+    setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1)
+
+    */
+
+    // Jab multiple updates karne hain ya current value pe depend hai
+    setCount(count => count + 1)
+    setCount( (count) => (count + 1) )
+    setCount( (count) => (count + 1) )
+    setCount( (count) => {return count + 1} )
+    setCount( (count) => {return count + 1} )
+    
+    
+  }
+  const removeValue = () => {
+      setCount(count => count - 1)
+      setCount(count => count - 1)
+      setCount(count => count - 1)
+  }
+  return (
+    <>
+      <button onClick={handleClick}>Add value : {count}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value : {count}</button>
+    </>
+  )
+}
+
+export default App
