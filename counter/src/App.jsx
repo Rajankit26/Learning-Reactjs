cdb4c93379c515bbccf2e0eac615a09c31db9bfb
Ankit Raj <142731001+Rajankit26@users.noreply.github.com>
2025-05-04 07:57:29 +0000

    Created reusable Counter component with independent state for each instance

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react"
 
+function Counter(){
+  const [count, setCount] = useState(10)
+
+  const plusOne = () => {
+    setCount(count + 1);
+  }
+  return (
+    <button onClick={plusOne}>Count : {count}</button>
+  )
+}
+
 function App() {
   const [count, setCounter] = useState(0)
   function addValue(){
@@ -9,6 +20,12 @@ function App() {
     <>
       <button onClick={addValue}
       >Count : {count}</button>
+    
+
+      <Counter />
+      <Counter />    
+      <Counter />
+      <Counter />
     </>
   )
 }
