32323846403ee7d301b81ca6e2e0a9c71ebd9d93
Ankit Raj <142731001+Rajankit26@users.noreply.github.com>
2025-05-17 08:14:24 +0000

    add Todo component to display title, description andd status

--- newTodo/frontend/todo/src/App.jsx
+++ newTodo/frontend/todo/src/App.jsx
@@ -6,9 +6,15 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
 
+  fetch("http://localhost:5000/api/v1/todo")
+    .then(async function(res) {
+      const json = await res.json();
+      setTodos(json.todos);
+    })
   return (
     <>
      <CreateTodo></CreateTodo>
+     <Todos todos={todos}></Todos>
     </>
   )
 }
--- newTodo/frontend/todo/src/Components/Todos.jsx
+++ newTodo/frontend/todo/src/Components/Todos.jsx
@@ -1,9 +1,25 @@
 
+/*
+    todos = [
+    {
+        title :
+        description :
+    },
+    ]
+*/
+
 
 export function Todos({todos}){
+
     return <>
-    <h1>title : {todos.title}</h1>
-    <h5>description : {todos.description}</h5>
-    <button>{todos.status === true ? "Completed" : "Mark as complete"}</button>
+    {
+        todos.map( (todo) => {
+            return <div key={todo._id}>
+            <h1>{todo.title}</h1>
+            <h4>{todo.description}</h4>
+            <button>{todo.status === true ? "Completed" : "Mark as done!"}</button>
+            </div>
+        } )
+    }
     </>
 }
